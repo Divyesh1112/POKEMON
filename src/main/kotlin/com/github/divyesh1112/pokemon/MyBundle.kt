fd1cb00d3cae56b4b7c33cce679d9ab296bb7306
GitHub Action <action@github.com>
2024-01-28 09:23:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/divyesh1112/pokemon/MyBundle.kt
+++ src/main/kotlin/com/github/divyesh1112/pokemon/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.divyesh1112.pokemon
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/divyesh1112/pokemon/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/divyesh1112/pokemon/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.divyesh1112.pokemon.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
